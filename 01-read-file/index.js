61e6fb783f449efb8974bdf532175834a4ca8a90
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-05-05 19:45:48 +0200

    fix: solved #1

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf8');
+
+readStream.on('data', (data) => {
+  console.log(data);
+});
+
+readStream.on('error', (err) => {
+  console.error(err);
+});
