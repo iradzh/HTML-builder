aadad73e46f3e76bdaa9e762381f2558396383c2
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-05-07 18:08:40 +0200

    fix: solved #3, #4

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`Files in ${folderPath}:`);
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (stats.isFile()) {
+        console.log(file);
+      }
+    });
+  });
+});
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+  const srcPath = path.join(__dirname, 'files');
+  const destPath = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.rm(destPath, { recursive: true });
+  } catch (error) {
+    // ignore
+  }
+
+  await fs.mkdir(destPath);
+
+  const files = await fs.readdir(srcPath);
+  for (const file of files) {
+    const srcFile = path.join(srcPath, file);
+    const destFile = path.join(destPath, file);
+    const stats = await fs.stat(srcFile);
+    if (stats.isDirectory()) {
+      await copyDir(srcFile, destFile);
+    } else {
+      await fs.copyFile(srcFile, destFile);
+    }
+  }
+}
+
+copyDir();
